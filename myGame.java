bffc2555a8e834217d80d282b49638c7cafb3855
AstraSpec <rizvan.faisal@gmail.com>
2024-03-04 22:20:35 +0000

    Update myGame.java

--- myGame.java
+++ myGame.java
@@ -3,6 +3,16 @@ public class myGame {
     public static void main(String[] args)
     {
         GameArena arena = new GameArena(1920,1080);
-        //test
+
+        Mario mario = new Mario(150, 100);
+
+        mario.addTo(arena);
+
+        while(true)
+        {
+            mario.update(arena);
+
+            arena.pause();
+        }
     }
 }
